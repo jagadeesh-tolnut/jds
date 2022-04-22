09ed474b466c4a2d48263d8d1baaa999311008c2
jagadeesh-tolnut <jagadeeshmouthouvel@gmail.com>
2022-04-22 10:53:27 +0530

    Queue member func added

--- jds/Queue.py
+++ jds/Queue.py
@@ -9,14 +9,18 @@ class Queue:
     def __len__(self):
         # returns length of queue
         return self.size
+
     def is_empty(self):
+        # returns boolean (True- empty, False- not empty)
         return self.size==0
 
     def enqueue(self,element):
+        # Inserting element in last position
         self.data.append(element)
         self.size += 1
 
     def dequeue(self):
+        # Removing and returning first element
         if self.is_empty():
             Exception("Queue is empty")
         else:
@@ -27,7 +31,15 @@ class Queue:
             return deq_ele
 
     def first(self):
+        # Returning the first element
         if self.is_empty():
             Exception("Queue is empty")
         else:
             return self.data[self.start]
+
+    def last(self):
+        # returning the last element
+        if self.is_empty():
+            Exception("Queue is empty")
+        else:
+            return self.data[(self.start + self.size)-1]
